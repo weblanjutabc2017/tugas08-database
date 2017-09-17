0bd245887c9f51bb68e105efa5604dfb3b9b42a5
zorahima <zorahima10@gmail.com>
2017-09-17 21:48:17 +0700

    Edited Update User 1 : tampilan dan conn

--- 08_AB_09459/updateUser.php
+++ 08_AB_09459/updateUser.php
@@ -1,10 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Bootstrap -->
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="asset/css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 <?php
-require_once 'koneksi.php';
+	require_once 'koneksi.php';
 
-$id = $_GET[id];
-$hapus = "delete from user where id='$id'";
-$query = mysqli_query($conn, $hapus) or die(mysql_error($conn));
-//kembali ke halaman user.php
-header('location:index.php?update=3');
-exit;
-?>
+	$user_id = $_GET["id"];
+	$sql_limit = "select * from user where user_id = $user_id";
+			$hasil = mysqli_query($conn, $sql_limit) or die(mysqli_error($conn));
+	$data = mysqli_fetch_array($hasil);
+	?>		
+</head>
+<body>
+	<div class="container">
+		<h1>Edit User</h1>
+		<hr>
+		<div class="row">
+		<form action="updateUser2.php" method="post">
+			<!-- left column -->
+
+			<!-- edit form column -->
+		<!-- 	<div class="col-md-9 personal-info">
+				<div class="alert alert-info alert-dismissable">
+					<a class="panel-close close" data-dismiss="alert">×</a> 
+					<i class="fa fa-coffee"></i>
+					This is an <strong>.alert</strong>. Use this to show important messages to the user.
+				</div> -->
+			<!-- 	<h3>user info</h3> -->
+			
+				
+					<div class="form-group">
+						<label class="col-lg-3 control-label">Username:</label>
+						<div class="col-lg-8">
+							<input class="form-control" type="text"  name = "username" value="<?php echo $data[1];?>">
+							<input class="form-control" type="text"  name = "id_username" value="<?php echo $data[0];?>">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">Password:</label>
+						<div class="col-md-8">
+							<input class="form-control" name = "password" type="Password" value="" >
+						</div>
+
+						<div class="form-group">
+								<label class="col-md-3 control-label">Label:</label>
+							<select class="form-control" name="level" id="level"  required">
+								<option value="1">Admin</option>
+								<option value="0">User</option>
+
+							</select>
+						</div>
+
+
+						<div class="form-group">
+							<label class="col-md-8 control-label"></label>
+							<div class="col-md-8">
+									  <button type="submit" name="simpan" class="btn btn-primary" onClick="return confirm('Apakah anda yakin data yang anda isikan sudah benar dan sesuai?')">Simpan</button>
+								<!-- <input type="button" class="btn btn-primary" value="Save Changes"> -->
+								<span></span>
+								<input type="reset" class="btn btn-default" value="Cancel">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<hr>
+	</body>
+	</html>
