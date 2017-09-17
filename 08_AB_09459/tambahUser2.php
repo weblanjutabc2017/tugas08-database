c37bf2dc4fd32e35df910284a149e342ae6b8952
zorahima <zorahima10@gmail.com>
2017-09-17 21:42:56 +0700

    Tambah Validasi

--- 08_AB_09459/tambahUser2.php
+++ 08_AB_09459/tambahUser2.php
@@ -1,17 +1,33 @@
 <?php
 require_once 'koneksi.php';
+include "config.php";
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 $level = $_POST['level'];
 $input = "insert into user(username, password, level) values ('$username', '$password', '$level')";
+
 if ($username == "" or $password == "") {
-    echo '<script type="text/javascript">
+	echo '<script type="text/javascript">
 	var answer = alert("Data masih belum lengkap")
-    window.location = "tambahUser.php";
-    </script>';
-} else {
+	window.location = "tambahUser.php";
+	</script>';
+
+
+
+}
+ else {
+ 		$cek_user=mysqli_num_rows(mysqli_query($conn,"SELECT * FROM user WHERE username='".$username."'"));
+			if ($cek_user > 0) {
+				echo '<script language="javascript">
+				alert ("User Sudah Ada Yang Menggunakan");
+				window.location="tambahUser.php";
+			</script>';
+		}
+		else {
 	$hasil = mysqli_query($conn, $input);    
 	header('location:index.php?update=2');    
+}
+
 }
 ?>
